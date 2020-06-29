f75bd1fdaa8897664817d69f4b99a7bd4cafeda8
Abacus7 <vladhuz@yahoo.com>
2020-06-28 21:48:08 -0400

    started AsyncTask and networking draft

--- app/src/main/java/com/example/android/newsfeedapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsfeedapp/MainActivity.java
@@ -2,13 +2,10 @@ package com.example.android.newsfeedapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,21 +13,45 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Tag for the log messages */
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    /** URL to query the Guardian dataset for news information */
+    private static final String WEBSITE_REQUEST_URL =
+            //"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
+            //"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-12-01&minmagnitude=7";
+            "https://content.guardianapis.com/search?api-key=ad2374d4-a9bb-46a9-9d41-0a3cb773578f";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Kick off an {@link AsyncTask} to perform the network request
+        TsunamiAsyncTask task = new TsunamiAsyncTask();
+        task.execute();
+
+
         // create list of articles
         final ArrayList<NewsArticle> articles = new ArrayList<>();
 
 
         TextView output = (TextView) findViewById(R.id.textView1);
-        String strJson= "{\"response\":{\"status\":\"ok\",\"userTier\":\"developer\",\"total\":2201483,\"startIndex\":1,\"pageSize\":10,\"currentPage\":1,\"pages\":220149,\"orderBy\":\"newest\"},\n" +
+        String strJson = "{\"response\":{\"status\":\"ok\",\"userTier\":\"developer\",\"total\":2201483,\"startIndex\":1,\"pageSize\":10,\"currentPage\":1,\"pages\":220149,\"orderBy\":\"newest\"},\n" +
                 "\"results\":[\n" +
                 "{\"id\":\"business/live/2020/jun/26/stocks-rise-despite-fed-warning-over-700bn-in-covid-19-loan-losses-business-live\",\n" +
                 "\"type\":\"liveblog\",\n" +
@@ -81,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
             String[] article_author = new String[jsonArray.length()];
 
             //Iterate the jsonArray and print the info of JSONObjects
-            for(int i=0; i < jsonArray.length(); i++){
+            for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
 
                 //int id = Integer.parseInt(jsonObject.optString("id").toString());
@@ -90,9 +111,9 @@ public class MainActivity extends AppCompatActivity {
                 String webTitle = jsonObject.optString("webTitle").toString();
                 String webUrl = jsonObject.optString("webUrl").toString();
 
-                data += "Node"+i+" : \n " +
+                data += "Node" + i + " : \n " +
                         "Section= " + sectionName +
-                        "\n Title= "+ webTitle +
+                        "\n Title= " + webTitle +
                         "\n Web= " + webUrl + " \n";
                 article_section[i] = sectionName;
                 article_title[i] = webTitle;
@@ -103,24 +124,30 @@ public class MainActivity extends AppCompatActivity {
                 boolean isFound = webTitle.indexOf("|") != -1;
 
                 if (isFound) {
-                    String author = webTitle.substring((webTitle.indexOf("|")+2));
+                    String author = webTitle.substring((webTitle.indexOf("|") + 2));
                     data += "\n Author: " + author + "\n ";
                 }
 
                 data += "\n Current Date: " + (new DateTimeFormat("")).getCurrentDate() + "\n";
-                data += "\n Current Raw Date: " + (new DateTimeFormat("")).getRawDateTime()+ "\n";
-                data += "\n Current Time: " + (new DateTimeFormat("")).getCurrentTime()+ "\n";
+                data += "\n Current Raw Date: " + (new DateTimeFormat("")).getRawDateTime() + "\n";
+                data += "\n Current Time: " + (new DateTimeFormat("")).getCurrentTime() + "\n";
 
-                articles.add(new NewsArticle(article_section[i],article_title[i],article_web[i], article_web[i],article_author[i]));
+                articles.add(new NewsArticle(article_section[i], article_title[i], article_web[i], article_web[i], article_author[i]));
 
             }
 
-           output.setText(data);
+            output.setText(data);
             //output.setText(articles.toString());
 
-        } catch (JSONException e ) {
+        } catch (JSONException e) {
             //Toast.makeText(this, "error" + e, Toast.LENGTH_LONG).show();
-            Log.e("Error", String.valueOf(e));}
+            Log.e("Error", String.valueOf(e));
+        }
+
+
+    }
+
+
 
         // the code below is to be removed.............................
         // .............................................................................
@@ -156,6 +183,189 @@ public class MainActivity extends AppCompatActivity {
 //                startActivity(websiteIntent);
 //            }
 //        });
+
+
+
+    /**
+     * Update the screen to display information from the given {@link NewsArticle}.
+     */
+    private void updateUi(NewsArticle earthquake) {
+//        // Display the earthquake title in the UI
+//        TextView titleTextView = (TextView) findViewById(R.id.title);
+//        titleTextView.setText(earthquake.title);
+//
+//        // Display the earthquake date in the UI
+//        TextView dateTextView = (TextView) findViewById(R.id.date);
+//        dateTextView.setText(getDateString(earthquake.time));
+//
+//        // Display whether or not there was a tsunami alert in the UI
+//        TextView tsunamiTextView = (TextView) findViewById(R.id.tsunami_alert);
+//        tsunamiTextView.setText(getTsunamiAlertString(earthquake.tsunamiAlert));
+    }
+
+    /**
+     * Returns a formatted date and time string for when the earthquake happened.
+     */
+    private String getDateString(long timeInMilliseconds) {
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMM yyyy 'at' HH:mm:ss z");
+        return formatter.format(timeInMilliseconds);
+    }
+
+    /**
+     * Return the display string for whether or not there was a tsunami alert for an earthquake.
+     */
+    private String getTsunamiAlertString(int tsunamiAlert) {
+        switch (tsunamiAlert) {
+            case 0:
+                return getString(R.string.alert_no);
+            case 1:
+                return getString(R.string.alert_yes);
+            default:
+                return getString(R.string.alert_not_available);
+        }
+    }
+
+    /**
+     * {@link AsyncTask} to perform the network request on a background thread, and then
+     * update the UI with the first earthquake in the response.
+     */
+    private class TsunamiAsyncTask extends AsyncTask<URL, Void, NewsArticle> {
+
+        @Override
+        protected NewsArticle doInBackground(URL... urls) {
+            // Create URL object
+            URL url = createUrl(WEBSITE_REQUEST_URL);
+
+            // Perform HTTP request to the URL and receive a JSON response back
+            String jsonResponse = "";
+            try {
+                jsonResponse = makeHttpRequest(url);
+            } catch (IOException e) {
+                // TODO Handle the IOException
+            }
+
+            // Extract relevant fields from the JSON response and create an {@link Event} object
+            NewsArticle earthquake = extractFeatureFromJson(jsonResponse);
+
+            // Return the {@link Event} object as the result fo the {@link TsunamiAsyncTask}
+            return earthquake;
+        }
+
+        /**
+         * Update the screen with the given earthquake (which was the result of the
+         * {@link TsunamiAsyncTask}).
+         */
+        @Override
+        protected void onPostExecute(NewsArticle earthquake) {
+            if (earthquake == null) {
+                return;
+            }
+
+            updateUi(earthquake);
+        }
+
+        /**
+         * Returns new URL object from the given string URL.
+         */
+        private URL createUrl(String stringUrl) {
+            URL url = null;
+            try {
+                url = new URL(stringUrl);
+            } catch (MalformedURLException exception) {
+                Log.e(LOG_TAG, "Error with creating URL", exception);
+                return null;
+            }
+            return url;
+        }
+
+        /**
+         * Make an HTTP request to the given URL and return a String as the response.
+         */
+        private String makeHttpRequest(URL url) throws IOException {
+            String jsonResponse = "";
+            HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
+
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                urlConnection.connect();
+                if (urlConnection.getResponseCode() == 200) {
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromStream(inputStream);
+                } else {
+                    Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+                }
+            } catch (IOException e) {
+                // TODO: Handle the exception
+                Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (inputStream != null) {
+                    // function must handle java.io.IOException here
+                    inputStream.close();
+                }
+            }
+            return jsonResponse;
+        }
+
+        /**
+         * Convert the {@link InputStream} into a String which contains the
+         * whole JSON response from the server.
+         */
+        private String readFromStream(InputStream inputStream) throws IOException {
+            StringBuilder output = new StringBuilder();
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+            return output.toString();
+        }
+
+        /**
+         * Return an {@link NewsArticle} object by parsing out information
+         * about the first earthquake from the input earthquakeJSON string.
+         */
+        private NewsArticle extractFeatureFromJson(String earthquakeJSON) {
+
+            // if the JSON string is empty or null, then return early.
+            if (TextUtils.isEmpty(earthquakeJSON)) {
+                return null;
+            }
+
+
+            try {
+                JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
+                JSONArray featureArray = baseJsonResponse.getJSONArray("features");
+
+                // If there are results in the features array
+                if (featureArray.length() > 0) {
+                    // Extract out the first feature (which is an earthquake)
+                    JSONObject firstFeature = featureArray.getJSONObject(0);
+                    JSONObject properties = firstFeature.getJSONObject("properties");
+
+                    // Extract out the title, time, and tsunami values
+                    String title = properties.getString("title");
+                    long time = properties.getLong("time");
+                    int tsunamiAlert = properties.getInt("tsunami");
+
+                    // Create a new {@link Event} object
+                    return new NewsArticle(title, time, tsunamiAlert);
+                }
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
+            }
+            return null;
+        }
 
     }
 }
